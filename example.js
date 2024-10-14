b761f444fac3fb44e6c7761bcd677ae02100c4cf
Muhad99 <145128166+Muhad99@users.noreply.github.com>
2024-10-14 07:59:35 +0100

    first commit

---
+++ example.js
@@ -0,0 +1,22 @@
+// function encodeStr() {
+//     console.log(btoa(str));
+// }
+
+// const str = "This is GeeksForGeeks";
+// encodeStr();
+
+// function decodeStr() {
+//     console.log(atob(str));
+// }
+
+// const str = "VGhpcyBpcyBHZWVrc0ZvckdlZWtz";
+// decodeStr();
+
+
+// var regex = /hello/;
+// var str = 'hello world';
+// var result = regex.test(str);
+// console.log(result);
+
+var regexGlobal = /abc/g;
+console.log(regexGlobal.test('abc abc'));
---
+++ first.js
@@ -0,0 +1,55 @@
+// let num = 4
+// console.log(num)
+
+//  let user = "Muhammed"
+//  console.log(user)
+
+
+// let radius = 5
+// const pi = 3.14
+// let area
+
+
+
+// area = pi * radius * radius 
+
+// console.log(area)
+
+
+
+// let data = 8          //number
+// let user ="Muhad"     //String
+// let bool = 3 < 4       // True or False
+// let username = null        // object
+// let name                 // undefineed
+
+// let num1 = 5
+// let num2 =  2
+
+// let result = num1 / num2
+// console.log(result);
+
+// var person = "john"
+// array
+
+// An array is a special variable, which can hold more than one value:
+
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// function myFunction(p1, p2) {
+//     return p1 * p2;
+//   }
+//   let x = myFunction(4, 3);
+
+// function myFunction(a, b) {
+//   return a * b;
+// }
+
+// const person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id       : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
